cbee181a68d93721e40be8b85949aab26824e082
Madison Snyder <mlsnyder@usc.edu>
2017-06-20 16:32:23 -0700

    reverting

--- dynamic/react/shared/components/consoleLiveData.jsx
+++ dynamic/react/shared/components/consoleLiveData.jsx
@@ -141,7 +141,16 @@ const PostHelper = ({action, endpoint, request, highlightedInputs, onConsoleTogg
     }
 };
 
+<<<<<<< HEAD
 const ConsoleLiveData = ({action, consoleLoading, highlightedInputs, path, request, response, onConsoleToggledFreeEdit, onConsoleToggledReadOnly, onRequestChanged, endpoint}) => {
+=======
+function checkFormatting(onRequestChanged, id) {
+    var text = document.getElementById('console_input');
+    onRequestChanged(id, text.value);
+};
+
+const ConsoleLiveData = ({ action, highlightedInputs, path, request, response, onRequestChanged, endpoint, onConsoleToggledFreeEdit, onConsoleToggledReadOnly}) => {
+>>>>>>> parent of 5c41df3... API CONSOLE DONE
     return (
         <div>
             <h5 className={'console-output-header'}>{'API Endpoint'}</h5>
@@ -167,12 +176,12 @@ const ConsoleLiveData = ({action, consoleLoading, highlightedInputs, path, reque
                         <div className={'json_error'}>
                             <h5>{'Incorrect JSON format'}</h5>
                         </div>: null}
-                                <div className={'code-snippet'}>{consoleLoading ? <div className={'loading-pulse'}></div> : <pre dangerouslySetInnerHTML={{__html: response ? syntaxHighlight(response.body) : ' '}} />}</div>
+                                <div className={'code-snippet'}>{endpoint.consoleLoading ? <div className={'loading-pulse'}></div> : <pre dangerouslySetInnerHTML={{__html: response ? syntaxHighlight(response.body) : ' '}} />}</div>
                     </div>
                 </div> :
                 <div>
                     <h5 className={'console-output-header'}>{'Response'}</h5>
-                            <div className={'code-snippet'}>{consoleLoading ? <div className={'loading-pulse'}></div> : <pre dangerouslySetInnerHTML={{__html: response ? syntaxHighlight(response.body) : ' '}} />}</div>
+                            <div className={'code-snippet'}>{endpoint.consoleLoading ? <div className={'loading-pulse'}></div> : <pre dangerouslySetInnerHTML={{__html: response ? syntaxHighlight(response.body) : ' '}} />}</div>
                     {/* eslint-enable react/no-danger */}
                 </div>
             }
